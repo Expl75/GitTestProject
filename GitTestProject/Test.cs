125946b3d63b39be86b714ade651befd884a12ad
Expl75 <51804030+Expl75@users.noreply.github.com>
2025-03-19 10:01:50 +0200

    Add Test class

---
+++ GitTestProject/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestProject
+{
+    internal class Test
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("This is test method");
+        }
+    }
+}
